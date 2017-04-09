ff348e0bf56d668b0eea61e2191a44a74afc9fc2
Alex Mos <alexander.mospan@gmail.com>
2017-04-09 22:26:48 +0300

    works without response

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 
 electron.app.on('ready', function () {
   let mainWindow = new electron.BrowserWindow({
-    width: 600,
+    width: 440,
     height: 800
   })
   mainWindow.loadURL(`file://${__dirname}/index.html`)
--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-// const picture = require('cat-picture')
-// const lightningImagePoly = require('lightning-image-poly')
-//
-// var src = picture.src
-// picture.remove()
-//
-// var viz = new lightningImagePoly('#visualization', null, [src], {hullAlgorithm: 'convex'})
-
-// const remote = require('electron').remote
-// const fs = require('fs')
-//
-// function save () {
-//   remote.getCurrentWindow().webContents.printToPDF({
-//     portrait: true
-//   }, function (err, data) {
-//     fs.writeFile('annotation.pdf', data, function (err) {
-//       if (err) alert('error generating pdf!' + err.message)
-//       else alert('pdf saved!')
-//     })
-//   })
-// }
-//
-// window.addEventListener('keydown', function (e) {
-//   if (e.keyCode == 80) save()
-// })
-
-
 const fs = require('fs-extra')
 const path = require('path')
 
-// const root = '/Users/ugo/группы\ 2017'
-const root = '/Volumes/WD/Группы\ 2017'
+let src, dest
 
-const src = path.join(root, process.argv[2])
-const dest = path.join(path.join(root, 'на вёрстку'), process.argv[3])
-
-// console.log(src)
-// console.log(dest)
+// преобразует строку с номерами в массив
+const numbersToArr = (str) => {
+  return str.split('\n').filter(num => num !== '')
+}
 
 // возвращает имя файла по номеру
 const nameBuild = (num) => {
@@ -61,16 +32,11 @@ const nameBuild = (num) => {
 
 // копирует файл
 const cp = (fileName) => {
-  let srcPath = path.join(src, fileName)
-  let destPath = path.join(dest, fileName)
-  // console.log(destPath)
-  fs.access(srcPath, fs.constants.R_OK, (err) => {
-    debugger;
+  fs.access(path.join(src, fileName), fs.constants.R_OK, (err) => {
     if (err) {
       console.log(`${fileName}: не найден`)
-      return false
     } else {
-      fs.copy(srcPath, destPath, (err) => {
+      fs.copy(path.join(src, fileName), path.join(dest, fileName), (err) => {
         if (err) {
           throw err
         } else {
@@ -81,28 +47,43 @@ const cp = (fileName) => {
   })
 }
 
-// проверка доступ и корректность путей
-fs.access(src, fs.constants.R_OK, (err) => {
-  if (err) {
-    console.log('невозможно прочитать исходные фотографии')
-  } else {
-    fs.access(dest, fs.constants.W_OK, (err) => {
-      if (err) {
-        console.log('невозможно записать фотографии')
-      } else {
-        fs.access(path.join(dest, 'photos.txt'), fs.constants.R_OK, (err) => {
-          if (err) {
-            console.log('не найден файл со списом фотографий')
-          } else {
-            // парсинг файла с номерами фотографий
-            fs.readFile(path.join(dest, 'photos.txt'), (err, data) => {
-              let numbers = data.toString().split('\n')
-              let fileNames = numbers.map(number => nameBuild(number))
-              fileNames.map(fileName => cp(fileName))
-            })
-          }
-        })
-      }
-    })
-  }
-})
+// проверка доступа и корректности путей
+const main = (fileNames) => {
+  fs.access(src, fs.constants.R_OK, (err) => {
+    if (err) {
+      console.log('невозможно прочитать исходные фотографии')
+    } else {
+      fs.access(dest, fs.constants.W_OK, (err) => {
+        if (err) {
+          console.log('невозможно записать фотографии')
+        } else {
+          // копирование файлов по очереди  
+          fileNames.map(fileName => cp(fileName))
+        }
+      })
+    }
+  })
+}
+
+// запись адресов папок
+document.querySelector('#input-src').onchange = function(e) {
+  document.querySelector('#input-src-name').value = e.target.files[0].name
+  src = e.target.files[0].path
+}
+
+document.querySelector('#input-dest').onchange = function(e) {
+  document.querySelector('#input-src-name').value = e.target.files[0].name
+  dest = e.target.files[0].path
+}
+
+// запуск программы по нажатию на кнопку Copy
+document.querySelector('button').onclick = function(e) {
+  e.preventDefault()
+  let numbersStr = document.querySelector('#photonumbers').value
+  // преобразуем строку с номерами в массив
+  let numbersArr = numbersToArr(numbersStr)
+  // массив номеров - в массив названий файлов
+  let fileNames = numbersArr.map(num => nameBuild(num)).sort()
+
+  main(fileNames)
+}
