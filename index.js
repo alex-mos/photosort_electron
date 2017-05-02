4b78af1d326c7f7ca7dff73ef53b3bead90255a9
Alex Mos <alexander.mospan@gmail.com>
2017-05-02 10:59:22 +0300

    add: copy/paste

--- index.js
+++ index.js
@@ -128,3 +128,48 @@ document.querySelector('.submit').onclick = function(e) {
     main(fileNames)
   }
 }
+
+
+// const electron = require('electron')
+// const Menu = require('electron').Menu
+//
+// const InputMenu = Menu.buildFromTemplate([
+//   {
+//     label: 'Undo',
+//     role: 'undo',
+//   }, {
+//     label: 'Redo',
+//     role: 'redo',
+//   }, {
+//     type: 'separator',
+//   }, {
+//     label: 'Cut',
+//     role: 'cut',
+//   }, {
+//     label: 'Copy',
+//     role: 'copy',
+//   }, {
+//     label: 'Paste',
+//     role: 'paste',
+//   }, {
+//     type: 'separator',
+//   }, {
+//     label: 'Select all',
+//     role: 'selectall',
+//   }
+// ])
+
+// document.body.addEventListener('contextmenu', (e) => {
+//     e.preventDefault()
+//     e.stopPropagation()
+//
+//     let node = e.target
+//
+//     while (node) {
+//         if (node.nodeName.match(/^(input|textarea)$/i) || node.isContentEditable) {
+//             InputMenu.popup(remote.getCurrentWindow())
+//             break
+//         }
+//         node = node.parentNode
+//     }
+// })
